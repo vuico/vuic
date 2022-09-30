1f5a1f13ae96839cd8683272cd1a9f878f6485a8
Eugene <40366303+Beelink@users.noreply.github.com>
2022-09-30 15:11:13 +0300

    Prepare repo to be published on npm

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
-export { default as Button } from "./Button/Button.vue";
-export { default as Input } from "./Input/Input.vue";
+import Button from "./Button/Button.vue";
+import Input from "./Input/Input.vue";
+
+export default { Button, Input };
